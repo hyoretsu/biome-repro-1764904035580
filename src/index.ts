95d8c2d542ef4bef2ff80aa3b496a596d32627bf
Aran Leite <hyoretsu@gmail.com>
2025-12-05 00:24:52 -0300

    bump

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-const a: number[] = [];
-a.forEach(b => b + 5);
+const a: number[] = []
+// `lint/suspicious/useIterableCallbackReturn` is set to 'info' in `base.json`. If you move that rule to `mongo.json` it'll suddenly start working.
+a.forEach(b => b + 5)
 
 let b: undefined | string
+// `lint/style/noNonNullAssertion` is set to 'off' in `base.json`
 console.log(b!)
