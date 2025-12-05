9732b7e02bd4dc13e087c6af5f0db48b1240f544
Aran Leite <hyoretsu@gmail.com>
2025-12-05 00:20:16 -0300

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-/**
- * If possible, put your minimal reproduction in this file, but feel free to
- * create other files elsewhere in the project if necessary.
- *
- * Once you have a minimal reproduction, commit it and push it to a public
- * GitHub repo.
- */
+const a: number[] = [];
+a.forEach(b => b + 5);
+
+let b: undefined | string
+console.log(b!)
